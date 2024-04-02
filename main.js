8304fac8005d92aa265f3131d236eb89f528bb87
Gustavo Alves da Silva <106679291+Gugochi@users.noreply.github.com>
2024-04-02 08:43:23 -0300

    M.L.M.B Teste

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const botoes = document.querySelectorAll ('.botao');
 const textos = document.querySelectorAll ('.aba-conteudo');
 
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivo01 = new Date ("2024-12-10T00:00:00");
+const tempoObjetivo02 = new Date ("2024-12-31T00:00:00");
+const tempoObjetivo03 = new Date ("2024-05-24T00:00:00");
+const tempoObjetivo04 = new Date ("2024-12-31T00:00:00");
+
+const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
+
+for (let i=0; i<tempos.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i])
+}
+
+let tempoAtual = new Date ()
+
 for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
 
@@ -13,3 +28,21 @@ for(let i=0; i<botoes.length; i++){
         textos[i].classList.add('ativo')
     }
 }
+
+function calculaTempo (tempoObjetivo) {
+
+    let tempoAtual = new Date()
+    let tempoFinal = tempoObjetivo - tempoAtual
+
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
+
+}   
