ad34df85d12857daafb31c63a4558b7e50e7e9b9
Gustavo Alves da Silva <106679291+Gugochi@users.noreply.github.com>
2024-04-23 08:02:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,61 @@
-const botoes = document.querySelectorAll ('.botao');
-const textos = document.querySelectorAll ('.aba-conteudo');
+const botoes = document.querySelectorAll('.botao');
+const textos = document.querySelectorAll('.aba-conteudo');
 
 const contadores = document.querySelectorAll(".contador");
 
-const tempoObjetivo01 = new Date ("2024-12-10T00:00:00");
-const tempoObjetivo02 = new Date ("2024-12-31T00:00:00");
-const tempoObjetivo03 = new Date ("2024-05-24T00:00:00");
-const tempoObjetivo04 = new Date ("2024-12-31T00:00:00");
+const tempoObjetivo01 = new Date("2024-12-10T00:00:00");
+const tempoObjetivo02 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo03 = new Date("2024-05-24T00:00:00");
+const tempoObjetivo04 = new Date("2024-12-31T00:00:00");
 
 const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
-for (let i=0; i<tempos.length; i++){
-    contadores[i].textContent = calculaTempo(tempos[i])
+function atualizaConometro() {
+
+    for (let i = 0; i < tempos.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i])
+    }
 }
 
-let tempoAtual = new Date ()
+function comecaCronometro() {
+    atualizaConometro();
+    setInterval(atualizaConometro, 1000)
+}
 
-for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick = function(){
+comecaCronometro();
 
-        for(j=0; j<botoes.length; j++){
+let tempoAtual = new Date()
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove('ativo')
             textos[j].classList.remove('ativo')
         }
-    
+
         botoes[i].classList.add('ativo')
         textos[i].classList.add('ativo')
     }
 }
 
-function calculaTempo (tempoObjetivo) {
+function calculaTempo(tempoObjetivo) {
 
     let tempoAtual = new Date()
     let tempoFinal = tempoObjetivo - tempoAtual
 
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
 
-    return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
-
+    if (tempoFinal > 0) {
+        return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
+    } else{
+        return "Prazo finalizado"
+    }
 }   
